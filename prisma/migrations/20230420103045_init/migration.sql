9bba1d2322fe565e8cb504347cddeb286da73952
DungNguyen <dung1506362@nuce.edu.vn>
2023-04-20 22:38:43 +0700

    update

--- prisma/migrations/20230420103045_init/migration.sql
+++ prisma/migrations/20230420103045_init/migration.sql
@@ -6,7 +6,27 @@
   - You are about to alter the column `level` on the `user` table. The data in that column could be lost. The data in that column will be cast from `Enum(EnumId(2))` to `VarChar(191)`.
 
 */
--- AlterTable
-ALTER TABLE `user` MODIFY `type` VARCHAR(191) NULL,
-    MODIFY `status` VARCHAR(191) NULL,
-    MODIFY `level` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NULL,
+    `full_name` VARCHAR(191) NULL,
+    `avatar` VARCHAR(191) NULL,
+    `type` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `user_id_social` VARCHAR(191) NULL,
+    `status` VARCHAR(191) NULL,
+    `level` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `last_login` DATETIME(3) NULL,
+    `last_logout` DATETIME(3) NULL,
+    `current_used_product` INTEGER NULL,
+    `has_password` BOOLEAN NOT NULL DEFAULT true,
+
+    UNIQUE INDEX `user_user_name_key`(`user_name`),
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
