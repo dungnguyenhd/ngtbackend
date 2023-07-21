82e4ab2be76c480c514f7d5bf7839ca3d5bf8d93
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-07-21 18:17:00 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // enable CORS
+  app.enableCors({
+    origin: ['https://ngtstudio.netlify.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000); // listen on the Vercel-provided port or 3000
 }
 bootstrap();
