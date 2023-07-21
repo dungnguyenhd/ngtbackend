14fe25197c887646e366118a24bfec4b087f906f
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-07-21 17:22:36 +0700

    update socket gateway

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -2,8 +2,6 @@ import {
   WebSocketGateway,
   WebSocketServer,
   OnGatewayDisconnect,
-  SubscribeMessage,
-  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { FriendshipService } from 'src/modules/friendship/friendship.service';
@@ -18,19 +16,26 @@ export class AppGateway implements OnGatewayDisconnect {
   handleConnection(client: Socket) {
     // Xử lý khi kết nối
     console.log('Client connected');
+
+    // Lấy userId từ thông báo kết nối của client
+    const userId = client.handshake.query.userId as string;
+
+    if (userId) {
+      // Cập nhật trạng thái người dùng thành "online"
+      this.friendService.updateUserActive(parseInt(userId), 'ONLINE');
+    }
   }
 
   handleDisconnect(client: Socket) {
     // Xử lý khi mất kết nối
     console.log('Client disconnected');
-  }
 
-  @SubscribeMessage('update_status') // Cập nhật tên sự kiện thành 'update_status'
-  updateStatus(@MessageBody() data: { userId: string; status: string }) {
-    // Xử lý yêu cầu đặt trạng thái vào database (ví dụ: "online"/"offline") với thông tin người dùng và trạng thái
-    console.log(`User '${data.userId}' is '${data.status}'`);
-    this.friendService.updateUserActive(parseInt(data.userId), data.status);
-    // Ví dụ gọi phương thức trong service để xử lý
-    // this.yourService.updateUserStatus(data.userId, data.status);
+    // Lấy userId từ thông báo kết nối của client
+    const userId = client.handshake.query.userId as string;
+
+    if (userId) {
+      // Cập nhật trạng thái người dùng thành "offline"
+      this.friendService.updateUserActive(parseInt(userId), 'OFFLINE');
+    }
   }
 }
